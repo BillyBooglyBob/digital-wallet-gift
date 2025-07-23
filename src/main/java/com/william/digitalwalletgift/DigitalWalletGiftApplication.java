abfca8b43ee9f81659232c455035df8980dbe405
BillyBooglyBob <123722533+BillyBooglyBob@users.noreply.github.com>
2025-07-24 00:42:08 +1000

    Initial setup

---
+++ src/main/java/com/william/digitalwalletgift/DigitalWalletGiftApplication.java
@@ -0,0 +1,13 @@
+package com.william.digitalwalletgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalWalletGiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalWalletGiftApplication.class, args);
+    }
+
+}
